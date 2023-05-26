67db250e51a8f98091724d01aa63991bff9a6aa7
kumsuzCode <mkumsuz1@gmail.com>
2023-05-26 16:52:33 +1000

    fix(nvim): Remove nvim submodule

---
+++ .config/nvim/lua/kumsuzM/colorschemes/catppuccin.lua
@@ -0,0 +1,6 @@
+require("catppuccin").setup({
+	flavour = "mocha", -- latte, frappe, macchiato, mocha
+})
+
+vim.cmd.colorscheme("catppuccin")
+-- default scheme is mocha which is the best one anyway
